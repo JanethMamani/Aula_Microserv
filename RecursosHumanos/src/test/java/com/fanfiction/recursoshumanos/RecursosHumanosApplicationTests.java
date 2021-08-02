631eb7fe393cca82ff4e4715431c251b4bf6a933
Janeth Mamani <janeth@ibm.com>
2021-08-02 11:44:25 -0300

    Projeto Recursos criado

---
+++ RecursosHumanos/src/test/java/com/fanfiction/recursoshumanos/RecursosHumanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanfiction.recursoshumanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecursosHumanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
