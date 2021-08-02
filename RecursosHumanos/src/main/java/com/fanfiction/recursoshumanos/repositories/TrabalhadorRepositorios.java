d89843de3820fd0c058668863af167ee4e1dc6eb
Janeth Mamani <janeth@ibm.com>
2021-08-02 16:10:19 -0300

    Implementação do projeto RecursosHumanos

---
+++ RecursosHumanos/src/main/java/com/fanfiction/recursoshumanos/repositories/TrabalhadorRepositorios.java
@@ -0,0 +1,9 @@
+package com.fanfiction.recursoshumanos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fanfiction.recursoshumanos.entidades.Trabalhador;
+
+public interface TrabalhadorRepositorios extends JpaRepository<Trabalhador, Long> {
+
+}
