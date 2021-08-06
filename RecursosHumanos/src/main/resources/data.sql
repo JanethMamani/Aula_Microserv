48690b371a8eabc6f3ac29b79b16a1c9a830f3a4
Janeth-Mamani <86673302+Janeth-M@users.noreply.github.com>
2021-08-06 12:22:05 -0300

    Delete RecursosHumanos directory

--- RecursosHumanos/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO tabela_trabalhador (nome, ganho) VALUES ('X', 300.0);
-INSERT INTO tabela_trabalhador (nome, ganho) VALUES ('Zero', 500.0);
-INSERT INTO tabela_trabalhador (nome, ganho) VALUES ('Axl', 400.0);
