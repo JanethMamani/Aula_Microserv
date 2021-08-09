3d2eac6e6953c19eb354d77dcc597a1d6afe0e7b
Janeth Mamani <janeth@ibm.com>
2021-08-09 11:19:31 -0300

    Projeto folha de pagamento criado

---
+++ RH-FolhaDePagamentos/src/main/java/com/fanfiction/rhfolhadepagamentos/RhFolhaDePagamentosApplication.java
@@ -0,0 +1,13 @@
+package com.fanfiction.rhfolhadepagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhFolhaDePagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhFolhaDePagamentosApplication.class, args);
+	}
+
+}
