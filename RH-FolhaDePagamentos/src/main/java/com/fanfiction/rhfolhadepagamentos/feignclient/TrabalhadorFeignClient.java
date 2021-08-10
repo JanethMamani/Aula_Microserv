045d3278a84855f15b85b73cc4a0dec78694f2be
Janeth Mamani <janeth@ibm.com>
2021-08-10 09:57:39 -0300

    Implementação de Feign Client

---
+++ RH-FolhaDePagamentos/src/main/java/com/fanfiction/rhfolhadepagamentos/feignclient/TrabalhadorFeignClient.java
@@ -0,0 +1,18 @@
+package com.fanfiction.rhfolhadepagamentos.feignclient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.fanfiction.rhfolhadepagamentos.entidades.Trabalhador;
+
+@Component
+@FeignClient(name = "rh-trabalhadores", url = "localhost:8001", path = "/trabalhadores")
+public interface TrabalhadorFeignClient {
+	
+	@GetMapping(value = "/{id}")
+	ResponseEntity<Trabalhador> findById(@PathVariable Long id);
+
+}
--- RH-FolhaDePagamentos/src/main/java/com/fanfiction/rhfolhadepagamentos/servico/PagamentosServico.java
+++ RH-FolhaDePagamentos/src/main/java/com/fanfiction/rhfolhadepagamentos/servico/PagamentosServico.java
@@ -1,33 +1,35 @@
 package com.fanfiction.rhfolhadepagamentos.servico;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.fanfiction.rhfolhadepagamentos.entidades.Pagamentos;
 import com.fanfiction.rhfolhadepagamentos.entidades.Trabalhador;
+import com.fanfiction.rhfolhadepagamentos.feignclient.TrabalhadorFeignClient;
 
 @Service
 public class PagamentosServico {
 	
+	/*//Do Rest Template associado no aplication properties
 	@Value("${rh-trabalhadores.host}")
 	private String trabalhadorHost;
 	
+	@Autowired//Para usar o Rest Template
+	private RestTemplate metodosRest;*/
+	
 	@Autowired
-	private RestTemplate metodosRest;
+	private TrabalhadorFeignClient clienteFeign;
 	
 	
 	public Pagamentos servPagamento(long trabalhadorId, int dias) {
-		Map<String, String> siteEntry = new HashMap<>();
-		siteEntry.put("id", "" + trabalhadorId);
-		
-		Trabalhador pessoa = metodosRest.getForObject(trabalhadorHost + "/trabalhadores/{id}", Trabalhador.class, siteEntry);
-		return new Pagamentos(pessoa.getNome(), pessoa.getGanhoDiario(), dias);
 		//return new Pagamentos("X", 400.0, dias); //Dado mockados ou por hard code
+		
+		/*Map<String, String> siteEntry = new HashMap<>();
+		siteEntry.put("id", "" + trabalhadorId);
+		Trabalhador pessoa = metodosRest.getForObject(trabalhadorHost + "/trabalhadores/{id}", Trabalhador.class, siteEntry);*/
+		
+		Trabalhador pessoa = clienteFeign.findById(trabalhadorId).getBody();
+		return new Pagamentos(pessoa.getNome(), pessoa.getGanhoDiario(), dias);
 		
 	}
 
