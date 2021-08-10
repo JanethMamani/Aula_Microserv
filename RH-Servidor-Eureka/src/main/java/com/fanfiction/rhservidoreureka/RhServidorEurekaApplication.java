58343dd90e6b4096da43bcae56e3f89ace2355e8
Janeth Mamani <janeth@ibm.com>
2021-08-10 16:05:28 -0300

    Projeto de Servidor Eureka criado

---
+++ RH-Servidor-Eureka/src/main/java/com/fanfiction/rhservidoreureka/RhServidorEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.fanfiction.rhservidoreureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhServidorEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhServidorEurekaApplication.class, args);
+	}
+
+}
---
+++ RH-Servidor-Eureka/src/test/java/com/fanfiction/rhservidoreureka/RhServidorEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanfiction.rhservidoreureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhServidorEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
