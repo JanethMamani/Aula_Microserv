1774ed3587a3151b6098eef2adb251687a544f23
Janeth Mamani <janeth@ibm.com>
2021-08-11 13:46:39 -0300

    Configurando o cliente Eureka - dois

--- RH-FolhaDePagamentos/src/main/java/com/fanfiction/rhfolhadepagamentos/RhFolhaDePagamentosApplication.java
+++ RH-FolhaDePagamentos/src/main/java/com/fanfiction/rhfolhadepagamentos/RhFolhaDePagamentosApplication.java
@@ -2,10 +2,10 @@ package com.fanfiction.rhfolhadepagamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "RH-Trabalhadores")
+@EnableEurekaClient // já inclui a annotation @RibbonClient(name = "RH-Trabalhadores") nas funções
 @EnableFeignClients
 @SpringBootApplication
 public class RhFolhaDePagamentosApplication {
