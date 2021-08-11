9bebba3e7d47335635fec671b61c992ffa89668d
Janeth Mamani <janeth@ibm.com>
2021-08-11 15:57:26 -0300

    Tolerancias do Hystrix

--- RH-FolhaDePagamentos/src/main/java/com/fanfiction/rhfolhadepagamentos/recursos/PagamentosRecursos.java
+++ RH-FolhaDePagamentos/src/main/java/com/fanfiction/rhfolhadepagamentos/recursos/PagamentosRecursos.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fanfiction.rhfolhadepagamentos.entidades.Pagamentos;
 import com.fanfiction.rhfolhadepagamentos.servico.PagamentosServico;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping(value = "/pagamentos")
@@ -17,10 +18,16 @@ public class PagamentosRecursos {
 	@Autowired
 	private PagamentosServico servico;
 	
+	@HystrixCommand(fallbackMethod = "getPagosAternativo")
 	@GetMapping(value = "/{trabalhadorId}/dias/{dias}")
 	public ResponseEntity<Pagamentos> getPagamento(@PathVariable Long trabalhadorId, @PathVariable Integer dias){
 		Pagamentos pagamento = servico.servPagamento(trabalhadorId, dias);
 		return ResponseEntity.ok(pagamento);
+	}
+	
+	public ResponseEntity<Pagamentos> getPagosAternativo(Long trabalhadorId, Integer dias){
+		Pagamentos solucaoMock = new Pagamentos("Sherlock", 200.0, dias);//Aqui podia colocar outra coisa no caso do servidor fora do ar
+		return ResponseEntity.ok(solucaoMock);
 	}
 
 }
