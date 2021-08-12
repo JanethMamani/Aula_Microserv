71fe573d958f1e61a68e60c8c48e00a3ae86ee41
Janeth Mamani <janeth@ibm.com>
2021-08-12 14:33:12 -0300

    Criação do servidor de configuração

---
+++ RH-Servidor-Configuration/src/main/java/com/fanfiction/rhservidorconfiguration/RhServidorConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.fanfiction.rhservidorconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhServidorConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhServidorConfigurationApplication.class, args);
+	}
+
+}
---
+++ RH-Servidor-Configuration/src/test/java/com/fanfiction/rhservidorconfiguration/RhServidorConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanfiction.rhservidorconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhServidorConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
