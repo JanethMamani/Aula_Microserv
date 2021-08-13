677f9903c7ac6a1fc2006f8a86c757f329bed4fc
Janeth Mamani <janeth@ibm.com>
2021-08-13 13:26:53 -0300

    Configuração de serviço RH-Trabalhadores como cliente do  Config Server

--- RH-Trabalhadores/src/main/java/com/fanfiction/rhtrabalhadores/recursos/TrabalhadorRecursos.java
+++ RH-Trabalhadores/src/main/java/com/fanfiction/rhtrabalhadores/recursos/TrabalhadorRecursos.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,11 +22,20 @@ public class TrabalhadorRecursos {
 	
 	private static Logger verPortaLogg = LoggerFactory.getLogger(TrabalhadorRecursos.class);
 	
+	@Value(value = "${safira.config}")
+	private String safiraConfig;
+	
 	@Autowired
 	private Environment envir;
 	
 	@Autowired  //injeta as dependencias da classe 
 	private TrabalhadorRepositorio repositorio;
+	
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs(){
+		verPortaLogg.info("CONFIG = " + safiraConfig);
+		return ResponseEntity.noContent().build();
+	}
 	
 	@GetMapping
 	//Um metodo tipo response entity que retornara uma lista com trabalhadores
